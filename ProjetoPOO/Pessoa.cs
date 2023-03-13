a1091e0f143b7b6eabc3b98630e30fabbc780a0f
GBFerro <giovani.ferro@usp.br>
2023-03-13 13:58:52 -0300

    Add project files.

---
+++ ProjetoPOO/Pessoa.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPOO
+{
+    internal class Pessoa
+    {
+
+        string _name;
+        string _lastName;
+
+        public Pessoa() { }
+
+        public int Idade { get; set; }
+
+
+
+        public void SetName(string name)
+        {
+            this._name = name;
+        }
+
+        public string GetName()
+        {
+            return this._name;
+        }
+
+        public void SetLastName(string name)
+        {
+            this._lastName = name;
+        }
+
+        public string GetLastName()
+        {
+            return this._lastName;
+        }
+
+        public string GetCompleteName()
+        {
+            return this._name + " " + this._lastName;
+        }
+
+
+
+    }
+}
+
---
+++ ProjetoPOO/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using ProjetoPOO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Pessoa person = new Pessoa();
+        Pessoa person2 = new Pessoa();
+
+        Console.WriteLine("Informe seu nome: ");
+        person.SetName(Console.ReadLine());
+
+        Console.WriteLine("Informe seu sobrenome: ");
+        person.SetLastName(Console.ReadLine());
+
+        Console.WriteLine("Informe sua idade: ");
+        person.Idade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Seja bem vindo a OO, {person.GetName()}");
+
+
+        Console.WriteLine($"Você tem {person.Idade} anos");
+
+        Console.WriteLine($"Seu nome completo é: {person.GetCompleteName()}");
+    }
+}
